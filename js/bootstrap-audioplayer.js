4cc444957a8453322f653ea616979984f1a72791
Long Ho <holevietlong@gmail.com>
2013-01-23 17:01:52 -0500

    gets the analyzer in there

--- js/bootstrap-audioplayer.js
+++ js/bootstrap-audioplayer.js
@@ -1,5 +1,5 @@
 /*jshint laxcomma:true expr:true */
-/*global webkitAudioContext:true AudioContext:true */
+/*global webkitAudioContext:true AudioContext:true console:true */
 // Bootstrap Audio Player
 // -----
 // v1.0.0
@@ -9,64 +9,80 @@
 (function ($) {
   'use strict';
 
+  //Audio context
+  var context;
+
+  if ('AudioContext' in window) {
+    context = new AudioContext();
+  } else if ('webkitAudioContext' in window) {
+    context = new webkitAudioContext();
+  }
+
   // `AudioModel` is a rudimentary wrapper that acts like a model,
   // but I don't wanna bring Backbone in since this POC is simple.
   // $audio is the `jQuery` wrapper for the `<audio>` element
-  var AudioModel = function ($audio) {
+  var AudioModel = function (index, song) {
     var self = this
+      , drawing
       , source
-      , context;
+      , analyzer
+      , audio
+      , $audio
+      , buffer;
 
-    if ('AudioContext' in window) {
-      context = new AudioContext();
-    } else if ('webkitAudioContext' in window) {
-      context = new webkitAudioContext();
-    }
-    
-    var _reset = function (index, song) {
+    var init = function () {
       if (!song) { return; }
+      audio = new Audio();
+      $audio = $(audio);
+      
       $audio.data('current', parseInt(index, 10));
       $audio.data('title', song.title);
-      //No webkitAudioContext, no fun stuff
-      if (!context) {
-        $audio.attr('src', song.url);
-        $audio.trigger('change:song', [index, song]);
-        this.setState($audio.data('state') || 'play');
-        return;
-      }
-      //Fun stuff is here
-      // $.ajax({
-      //   url: song.url,
-      //   datatype: 'arraybuffer',
-      //   success: function (data) {
-          
-      //   }
-      // });
-      var request = new XMLHttpRequest();
-      request.open('GET', song.url, true);
-      request.responseType = 'arraybuffer';
+      $audio.attr('src', song.url);
+      $audio.trigger('change:song', [index, song]);
+      self.setState($audio.data('state') || 'play');
 
-      // Decode asynchronously
-      request.onload = function() {
-        context.decodeAudioData(request.response, function(buffer) {
-            source = context.createBufferSource();
-            source.buffer = buffer;
-            source.connect(context.destination);
-            source.noteOn(0);
-          });
-      };
-      request.send();
+      $audio
+        .on('loadedmetadata', function () {
+          if (source) { return; }
+          analyzer = context.createAnalyser();
+          analyzer.smoothingTimeConstant = 0.85;
+
+          source = context.createMediaElementSource(self.getDOMAudio());
+          source.connect(analyzer);
+          analyzer.connect(context.destination);
+        })
+        .on('ended', function () {
+          self.fetch(self.getCurrent() + 1);
+        })
+        .data('state', 'pause');
     };
 
-    $audio.on('ended', function () {
-      self.fetch(self.getCurrent() + 1);
-    }).data('state', 'pause');
+    function drawSpectrum() {
+      if (!analyzer) { return; }
+      var canvas = $('canvas')[0];
+      var ctx = canvas.getContext('2d');
+      var width = canvas.width;
+      var height = canvas.height;
+      var bar_width = 10;
+
+      ctx.clearRect(0, 0, width, height);
+
+      var freqByteData = new Uint8Array(analyzer.frequencyBinCount);
+      analyzer.getByteFrequencyData(freqByteData);
+
+      var barCount = Math.round(width / bar_width);
+      for (var i = 0; i < barCount; i++) {
+          var magnitude = freqByteData[i];
+          // some values need adjusting to fit on the canvas
+          ctx.fillRect(bar_width * i, height, bar_width - 2, -magnitude + 60);
+      }
+    }
 
     this.setState = function (state) {
       if (state === 'pause') {
-        this.getDOMAudio().pause();
+        this.pause();
       } else {
-        this.getDOMAudio().play();
+        this.play();
       }
       
       $audio.data('state', state);
@@ -78,16 +94,33 @@
     this.getCurrent = function () {
       return $audio.data('current');
     };
-    this.fetch = function (index, song) {
-      _reset.call(this, index, song);
-      //Do a AJAX to get arraybuffer here.
-    };
+    
     this.getDOMAudio = function () {
       return $audio.get(0);
     };
     this.getSource = function () {
       return source;
     };
+
+    this.play = function () {
+      clearInterval(drawing);
+      drawing = setInterval(drawSpectrum, 50);
+      return this.getDOMAudio().play();
+    };
+    this.pause = function () {
+      clearInterval(drawing);
+      return this.getDOMAudio().pause();
+    };
+    this.getAudio = function () {
+      return $audio;
+    };
+    this.remove = function () {
+      this.pause();
+      $audio.unbind();
+      return $audio.remove();
+    };
+
+    init();
   };
 
   var AudioPlayer = function (el, opts) {
@@ -95,6 +128,7 @@
     var self = this;
     this.options = opts;
     this.$el = $(el);
+    
     this.dom = {
       $next: this.$el.find('.playback .next'),
       $prev: this.$el.find('.playback .prev'),
@@ -105,41 +139,15 @@
       $timer: this.$el.find('.timer'),
       $slider: this.$el.find('.slider'),
       $handle: this.$el.find('.handle'),
-      $title: this.$el.find('.title'),
-      $audio: this.$el.find('audio')
+      $title: this.$el.find('.title')
     };
-    this.model = new AudioModel(this.dom.$audio);
-    this.dom.$audio
-      .on('change:song', function (ev, index, song) {
-        self.dom.$title.text(song.title);
-      })
-      .on('change:state', function (ev, state) {
-        return self.dom.$play.html('<i class="icon-' + (state === 'play' ? 'pause' : 'play') + '"></i>');
-      })
-      .on('progress', function () {
-        var buffered = this.buffered.end(0);
-        if (!buffered) return;
-        var loaded = parseInt(((buffered / this.duration) * 100) + 3, 10);
-        self.dom.$loader.css('width', loaded + '%');
-      })
-      .on('timeupdate', function () {
-        var rem = parseInt(this.duration - this.currentTime, 10),
-        pos = Math.floor((this.currentTime / this.duration) * 100),
-        mins = Math.floor(rem/60, 10),
-        secs = rem - mins * 60;
-
-        self.dom.$timer.text('-' + mins + ':' + (secs > 9 ? secs : '0' + secs));
-        // if (!manualSeek) { options.handle.css({left: pos + '%'}); }
-        // if (!loaded) {
-        //   loaded = true;
-        // }
-      });
 
     this.current = 0;
+    this.model = new AudioModel(this.current, this.options.songs[this.current]);
     this.dom.$next.click($.proxy(this.next, this));
     this.dom.$prev.click($.proxy(this.prev, this));
     this.dom.$play.click($.proxy(this.playToggle, this));
-    this.model.fetch(this.current, this.options.songs[this.current]);
+    this.configure();
 
     opts.autoplay && this.play();
 
@@ -157,7 +165,9 @@
       } else {
         this.current++;
       }
-      this.model.fetch(this.current, this.options.songs[this.current]);
+      this.model.remove();
+      this.model = new AudioModel(this.current, this.options.songs[this.current]);
+      this.configure();
     },
     prev: function (e) {
       e && e.preventDefault();
@@ -169,7 +179,9 @@
       } else {
         this.current--;
       }
-      this.model.fetch(this.current, this.options.songs[this.current]);
+      this.model.remove();
+      this.model = new AudioModel(this.current, this.options.songs[this.current]);
+      this.configure();
     },
     playToggle: function (e) {
       e && e.preventDefault();
@@ -190,6 +202,34 @@
     },
     pause: function (e) {
       this.model.getState() === 'play' && this.playToggle();
+    },
+    configure: function () {
+      var self = this;
+      this.model.getAudio()
+        .on('change:song', function (ev, index, song) {
+          self.dom.$title.text(song.title);
+        })
+        .on('change:state', function (ev, state) {
+          return self.dom.$play.html('<i class="icon-' + (state === 'play' ? 'pause' : 'play') + '"></i>');
+        })
+        .on('progress', function () {
+          var buffered = this.buffered.end(0);
+          if (!buffered) return;
+          var loaded = parseInt(((buffered / this.duration) * 100) + 3, 10);
+          self.dom.$loader.css('width', loaded + '%');
+        })
+        .on('timeupdate', function () {
+          var rem = parseInt(this.duration - this.currentTime, 10),
+          pos = Math.floor((this.currentTime / this.duration) * 100),
+          mins = Math.floor(rem/60, 10),
+          secs = rem - mins * 60;
+
+          self.dom.$timer.text('-' + mins + ':' + (secs > 9 ? secs : '0' + secs));
+          // if (!manualSeek) { options.handle.css({left: pos + '%'}); }
+          // if (!loaded) {
+          //   loaded = true;
+          // }
+        });
     }
   };
 
